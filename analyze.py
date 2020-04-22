8ed1c970ed038f2d09792c7df0313bb81de92b27
ogatodesuyo <tenten_flog@icloud.com>
2020-04-22 22:54:52 +0900

    ライブラリの読み込みとテストサーバーの起動処理

---
+++ analyze.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+from collections import Counter
+
+#グラフ化用のライブラリ読み込み
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+#テスト用サーバー起動
+app.run(debug=True,host="0.0.0.0",port="8888")
+
+localhost:8888
