f8ff8f4e6f97460924d7f54b3b237b279a94b29b
ogatodesuyo <tenten_flog@icloud.com>
2020-04-22 22:55:46 +0900

    csvファイルの読み込みとデータの結合

--- analyze.py
+++ analyze.py
@@ -8,7 +8,14 @@ from collections import Counter
 import matplotlib
 import matplotlib.pyplot as plt
 
+#CSV読み込み
+apr_df = pd.read_csv("201911tweet.csv")
+may_df = pd.read_csv("201912tweet.csv")
+jun_df = pd.read_csv("202001tweet.csv")
+jul_df = pd.read_csv("202002tweet.csv")
 
+#結合
+combine = [apr_df, may_df, jun_df, jul_df]
 
 
 
